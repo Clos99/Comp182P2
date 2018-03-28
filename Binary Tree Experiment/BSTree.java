ae2857d2a4119c41d4365b5a72ee55b2cf6fa689
Clos99 <36385304+Clos99@users.noreply.github.com>
2018-03-28 13:56:53 -0700

    3/28/18 lab changes

--- Binary Tree Experiment/BSTree.java
+++ Binary Tree Experiment/BSTree.java
@@ -6,7 +6,25 @@ public class BSTree {
 	{
 		root = null;
 	}
-	
+	private void balTree(int[] arr) {
+
+	     int mid = arr.length / 2;
+	     int[] temporary = new int[mid];
+	     root.insert(mid);// this is the starting point of the balance tree
+		for (int i = 0; i < mid; i++) {//stores the first half the array into temp and goes through balTree again
+			temporary[i] = arr[i];
+			if(i == mid-1) {
+				balTree(temporary);
+			}
+		}
+		
+		for(int i = mid; i < arr.length; i++) {
+			temporary[i] = arr[i];
+			if(i == arr.length - 1)
+				balTree(temporary);
+		}
+
+	}
 	public void insert(int n) {
 		root.insert(n);
 	}
