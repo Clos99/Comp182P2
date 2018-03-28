2cf7cdd3d074bcbb14aec9276fd3c8fb235897ed
Clos99 <36385304+Clos99@users.noreply.github.com>
2018-03-28 14:05:51 -0700

    Update BSTree.java

--- Binary Tree Experiment/BSTree.java
+++ Binary Tree Experiment/BSTree.java
@@ -6,7 +6,7 @@ public class BSTree {
 	{
 		root = null;
 	}
-	private void balTree(int[] arr) {
+	private void balTree(int[] arr, BSTNode obj) {
 
 	     int mid = arr.length / 2;
 	     int[] temporary = new int[mid];
@@ -14,14 +14,14 @@ public class BSTree {
 		for (int i = 0; i < mid; i++) {//stores the first half the array into temp and goes through balTree again
 			temporary[i] = arr[i];
 			if(i == mid-1) {
-				balTree(temporary);
+				balTree(temporary, root);
 			}
 		}
 		
 		for(int i = mid; i < arr.length; i++) {
 			temporary[i] = arr[i];
 			if(i == arr.length - 1)
-				balTree(temporary);
+				balTree(temporary,root);
 		}
 
 	}
