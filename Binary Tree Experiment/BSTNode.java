d548be4e6277c7f1e972e4b9257118714079bee9
Clos99 <36385304+Clos99@users.noreply.github.com>
2018-03-28 14:03:31 -0700

    3/28/18

--- Binary Tree Experiment/BSTNode.java
+++ Binary Tree Experiment/BSTNode.java
@@ -10,22 +10,22 @@ public class BSTNode extends Element {
 	 this.right = null;
 	 this.left = null;
  }
- protected void insert(int n) {
+ protected void insert(int n, BSTNode obj) {
 	 if(this.compareTo(n) > 0) { //this is the case if key is greater than int n
 		 if(left == null)
 		 {
 			 left = new BSTNode(n);//if left is null then n becomes the new key to the left node
 		 }else  if(this.left != null && this.left.compareTo(n) > 0) {
-			 (this.left).insert(n);//if left.key is greater than n then it goes through the method once again but this time "left.key" is the main key
+			 (this.left).insert(n, obj);//if left.key is greater than n then it goes through the method once again but this time "left.key" is the main key
 		 }
 	 }
 	 if(this.compareTo(n) < 0) {//if key is less than n
 		 if(this.right == null) {//if there is no right n becomes the new right
 			 this.right = new BSTNode(n);
 		 }else if(this.right != null && this.right.compareTo(n) > 0) {//if n is less than right it goes through the method again intill it gets inserted
-			 (this.right).insert(n);
+			 (this.right).insert(n,obj);
 		 }else if(this.right != null && this.right.compareTo(n) < 0) {//if n is greater than "right" than it goes through the method again until it goes finds the right spot to go
-			 (this.right).insert(n);
+			 (this.right).insert(n,obj);
 		 }
 		 
 	 }
