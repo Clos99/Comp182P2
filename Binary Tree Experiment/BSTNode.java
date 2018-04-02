adb8a16d4494a4056a07fb36dc7908c5298b9b3f
Clos99 <36385304+Clos99@users.noreply.github.com>
2018-04-02 12:48:33 -0700

    Add files via upload

--- Binary Tree Experiment/BSTNode.java
+++ Binary Tree Experiment/BSTNode.java
@@ -1,65 +1,62 @@
 
 public class BSTNode extends Element {
- private int key;
- private BSTNode right, left;
-
- 
- public BSTNode(int k) {
-	 this.key = k;
-	 
-	 this.right = null;
-	 this.left = null;
- }
- protected void insert(int n, BSTNode obj) {
-	 if(this.compareTo(n) > 0) { //this is the case if key is greater than int n
-		 if(left == null)
-		 {
-			 left = new BSTNode(n);//if left is null then n becomes the new key to the left node
-		 }else  if(this.left != null && this.left.compareTo(n) > 0) {
-			 (this.left).insert(n, obj);//if left.key is greater than n then it goes through the method once again but this time "left.key" is the main key
-		 }
-	 }
-	 if(this.compareTo(n) < 0) {//if key is less than n
-		 if(this.right == null) {//if there is no right n becomes the new right
-			 this.right = new BSTNode(n);
-		 }else if(this.right != null && this.right.compareTo(n) > 0) {//if n is less than right it goes through the method again intill it gets inserted
-			 (this.right).insert(n,obj);
-		 }else if(this.right != null && this.right.compareTo(n) < 0) {//if n is greater than "right" than it goes through the method again until it goes finds the right spot to go
-			 (this.right).insert(n,obj);
-		 }
-		 
-	 }
- }
- 
- protected void delete(int n) {
-	 BSTNode temp;
-		//deletes BSTNode
-	 //insert code that deletes stuff
- }
+	private int key;
+	private BSTNode right, left;
 	
-	public int minVal() {//this is for when you want to delete a node but the node has a left leaf so this would return the smalles value in the left leaf.
-		if(this.left == null) {
-			return this.key;
-		}
-		else
-			return left.minVal();
+	//default constructor
+	public BSTNode() {
+		this.key = 0;
+		this.left = null;
+		this.right = null;
 	}
- //for this compareTo it forced me to change "int" to "Integer" in order for this to not give me an error msg but i dont know what the difference is
- public Integer compareTo(Integer n) {
-		if(this.key > n) {
+	//constructor
+	public BSTNode(int k) {
+		this.key = k;
+
+		this.right = null;
+		this.left = null;
+	}
+	
+	//setter
+	public void setLeft(BSTNode n) {
+		this.left = null;
+	}
+	
+	public void setRight(BSTNode n) {
+		this.right = null;
+	}
+	
+	public void setKey(int k) {
+		this.key = k;
+	}
+	
+	//getters
+	public BSTNode getLeft() {
+		return this.left;
+	}
+	
+	public BSTNode getRight() {
+		return this.right;
+	}
+	
+	public int getKey() {
+		return this.key;
+	}
+	
+
+
+
+
+	// for this compareTo it forced me to change "int" to "Integer" in order for
+	// this to not give me an error msg but i dont know what the difference is
+	public Integer compareTo(Integer n) {
+		if (this.key > n) {
 			return 1;
 		}
-		if(this.key < n) {
+		if (this.key < n) {
 			return -1;
-	 }
-	 return 0;
-}
+		}
+		return 0;
+	}
 
 }
-
-
-
-
-
- 
-
--- Binary Tree Experiment/BSTree.java
+++ Binary Tree Experiment/BSTree.java
@@ -1,20 +1,57 @@
-//I honestly havent touched this class at all therefore it has the least amount of code
+
 public class BSTree {
-	BSTNode root;
+	private BSTNode root;
 	
+	
+	
+	//constructor
 	public BSTree()
 	{
 		root = null;
 	}
-	private void balTree(int[] arr, BSTNode obj) {
+	
+	public boolean emptyRoot() {//checks if the root is empty or not
+		if(root == null) {
+			return true;
+		}
+		return false;
+	}
+	
 
-	     int mid = arr.length / 2;
-	     int[] temporary = new int[mid];
-	     root.insert(mid);// this is the starting point of the balance tree
-		for (int i = 0; i < mid; i++) {//stores the first half the array into temp and goes through balTree again
+	//insertion methods
+	public void insert(int k) {
+		root = insert(root,k);
+	}
+	
+		
+	
+	private BSTNode insert(BSTNode n, int k) {
+		if(n == null) {
+			n = new BSTNode(k);
+		}
+		else if(n.compareTo(k) > 0) {//this is the case when n is greater than k
+			n.setLeft(insert(n.getLeft(), k));
+		}
+		else if(n.compareTo(k) < 0) {
+			n.setRight(insert(n.getRight(), k));
+		}
+		
+			return n;
+	}
+	
+	
+	
+	
+/*	private void balTree(int[] arr,BSTNode obj) {
+
+		int mid = arr.length / 2;
+		int[] temporary = new int[mid];
+		root.insert(mid,root);// this is the starting point of the balance tree
+
+		for (int i = 0; i < mid; i++) {
 			temporary[i] = arr[i];
 			if(i == mid-1) {
-				balTree(temporary, root);
+				balTree(temporary,root);
 			}
 		}
 		
@@ -25,12 +62,26 @@ public class BSTree {
 		}
 
 	}
-	public void insert(int n) {
-		root.insert(n);
+*/
+	private void randTree() {
+
 	}
 	
+	
+	
+	
+	
+	
+	
+	
 	public void delete(int n) {
-		root.delete(n);
+		
+		root = delete(n,root);
+	}
+	
+	private BSTNode delete(int k, BSTNode n) {
+		//delete code
+		return;
 	}
 
 }
